9b28aa1792631d9451a380606c2bb4c7d5b639e0
inthezonewithme <158035989+inthezonewithme@users.noreply.github.com>
2025-05-21 09:39:12 +0300

    Made the login page

--- src/main/java/com/example/autoaxis/MainApplication.java
+++ src/main/java/com/example/autoaxis/MainApplication.java
@@ -10,8 +10,8 @@ import java.io.IOException;
 public class MainApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("CreateAccount.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 800, 600);
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("LoginPage.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 900, 600);
         stage.setTitle("AutoAxis");
         stage.setScene(scene);
         stage.setResizable(false);
